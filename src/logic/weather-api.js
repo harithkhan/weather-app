f8332461da6340c21e5fa415f143c29a189c19a2
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-06 16:31:54 +0800

    docs: reset project to include celcius conversions Reset entire project becuase celcius conversion was not considered up to this point. Project also resetted to give author a chance to revise concepts of asynchronous functions.

--- src/logic/weather-api.js
+++ src/logic/weather-api.js
@@ -4,19 +4,3 @@ function getApiUrl(location) {
     return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`;
 }
 
-export async function getWeatherData(location) {
-    try {
-        const response = await fetch(getApiUrl(location));
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const weatherData = await response.json();
-        if (!weatherData || !weatherData.currentConditions) {
-            throw new Error(`No valid weather data found for ${location}`);
-        }
-        return weatherData;
-    } catch (error) {
-        console.error(`Error fetching weather data for ${location}:`, error);
-        return null;
-    }
-}
