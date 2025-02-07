46c1fdb7eb9cbddf2e1392d7545ebc1e1701c8ec
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-07 08:58:03 +0800

    feat: add function getHourlyWeather

--- src/logic/parse-weather-data.js
+++ src/logic/parse-weather-data.js
@@ -5,7 +5,7 @@ export async function getCurrentWeather(location) {
         const rawWeatherData = await getRawWeatherData(location);
         if (!rawWeatherData) {
             throw new Error(`Could not obtain current weather for ${location}`);
-        }
+        };
         const currentWeather = {
             address: rawWeatherData.address,
             description: rawWeatherData.description,
@@ -18,12 +18,36 @@ export async function getCurrentWeather(location) {
             precipProb: rawWeatherData.currentConditions.precipprob,
             sunrise: rawWeatherData.currentConditions.sunrise,
             sunset: rawWeatherData.currentConditions.sunset
-        }
+        };
+        console.log(rawWeatherData);
         console.log(currentWeather);
         return(currentWeather);
     }
     catch (error){
         console.error(error);
         return null;
+    }
+}
+
+export async function getHourlyWeather(location) {
+    try {
+        const rawWeatherData = await getRawWeatherData(location);
+        if (!rawWeatherData) {
+            throw new Error(`Could not obtain hourly weather for ${location}`);
+        };
+        const rawHourlyWeatherData = rawWeatherData.days[0].hours;
+        const hourlyWeather = rawHourlyWeatherData.map((hourData) => ({
+                datetime: hourData.datetime,
+                conditions: hourData.conditions,
+                temp: hourData.temp,
+                feelsLike: hourData.feelslike,
+                icon: hourData.icon,
+                precipProb: hourData.precipprob
+        }));
+        console.log(hourlyWeather);
+        return hourlyWeather;
+    } catch (error) {
+        console.error(error);
+        return null
     }
 }
