ef4fd36b6db2aa1c60cc749b8d095ca580c09279
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-07 13:59:27 +0800

    feat: add function displayCurrentWeather Add function displayCurrentWeather in new module display-current-weather.js with query selectors to reference appropriate current weather HTML elements. Population logic of the elements to be added later.

---
+++ src/display/display-current-weather.js
@@ -0,0 +1,14 @@
+export async function displayCurrentWeather(currentWeather) {
+    const currentIconContainer = document.querySelector(".current-icon-container");
+    const locationContainer = document.querySelector(".location-container");
+    const tempContainer = document.querySelector(".temp-container");
+    const feelsLikeContainer = document.querySelector(".feels-like-container");
+    const humidityContainer = document.querySelector(".humidity-container");
+    const precipContainer = document.querySelector(".precip-container");
+    const uvIndexContainer = document.querySelector(".uvindex-container");
+    const conditionsContainer = document.querySelector(".conditions-container");
+    const descriptionContainer = document.querySelector(".description-container");
+    const sunriseContainer = document.querySelector(".sunrise-container");
+    const sunsetContainer = document.querySelector(".sunset-container");
+
+}
