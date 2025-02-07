3c57ce74ac7e9891c377079ce665b94a4c3a8413
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-07 08:16:42 +0800

    feat: add function getRawWeatherData

--- src/logic/weather-api.js
+++ src/logic/weather-api.js
@@ -4,3 +4,18 @@ function getApiUrl(location) {
     return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`;
 }
 
+export async function getRawWeatherData(location) {
+    try {
+        const apiUrl = getApiUrl(location);
+        const apiResponse = await fetch(apiUrl);
+        if (!apiResponse.ok) {
+            throw new Error("Bad Request");
+        }
+        const rawWeatherData = await apiResponse.json();
+        console.log(rawWeatherData)
+        return rawWeatherData
+    } catch (error) {
+        console.error(`Could not obtain weather data for ${location},`, error);
+        return null;
+    }
+}
