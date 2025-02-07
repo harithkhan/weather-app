fcd8610590e973e3ab476a258dd091f7c66afbe1
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-07 22:06:20 +0800

    feat: add left scroll to hourly weather display

--- src/display/display-hourly-weather.js
+++ src/display/display-hourly-weather.js
@@ -40,7 +40,6 @@ export function displayHourlyWeather(hourlyWeather) {
 function handleRightArrowClick() {
     const carousel = document.querySelector(".carousel");
     const currentRightStyle = carousel.style.right;
-    
     if (!currentRightStyle || currentRightStyle === "0rem") {
         carousel.style.right = "20rem";
     }
@@ -50,7 +49,20 @@ function handleRightArrowClick() {
     }
 }
 
+function handleLeftArrowClick() {
+    const carousel = document.querySelector(".carousel");
+    const currentRightStyle = carousel.style.right;
+    if (currentRightStyle && currentRightStyle !== "0rem") {
+        const currentRightStyleNumber = parseFloat(currentRightStyle.match(/\d+(\.\d+)?/)[0]);
+        carousel.style.right = `${currentRightStyleNumber - 20}rem`;
+    }
+
+}
+
 export function attachArrowButtonEventListeners() {
     const rightArrowButton = document.getElementById("right-button");
     rightArrowButton.addEventListener("click", handleRightArrowClick);
+
+    const leftArrowButton = document.getElementById("left-button");
+    leftArrowButton.addEventListener("click", handleLeftArrowClick);
 }
